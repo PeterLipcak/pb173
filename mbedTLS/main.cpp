5166c758b89aee83cdbdc4db4a07fbf8c6ce7c8b
sugy77 <lipcakpeter@gmail.com>
2016-02-27 15:09:36 +0100

    sha probably working well

--- mbedTLS/main.cpp
+++ mbedTLS/main.cpp
@@ -8,28 +8,50 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-//#include <mbedtls/aes.h>
+#include <mbedtls/aes.h>
+#include <mbedtls/sha512.h>
 
 using namespace std;
 
-void read_file(){
+mbedtls_aes_context aes;
+
+unsigned char key[32];
+unsigned char iv[16];
+
+unsigned char input [128];
+unsigned char output[128];
+
+size_t input_len = 40;
+size_t output_len = 0;
+
+void generateSHA(string file){
+    int length = file.length();
+    unsigned char output[64];
+    mbedtls_sha512((const unsigned char *)file.c_str(), length, output, 0);
+    
+    cout << "SHA2-512 = " << output << endl;
+}
+string read_file(){
     ifstream ifs ("test.txt", std::ifstream::in);
     
-    char c = ifs.get();
+    string file = "";
+    
+    file = ifs.get();
 
     while (ifs.good()) {
-        std::cout << c;
-        c = ifs.get();
+        file += ((char)ifs.get());
     }
 
-    ifs.close();    
+    ifs.close();  
+    
+    return file;
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    read_file();
+    generateSHA(read_file());
     return 0;
 }
 
